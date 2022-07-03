84b53a3ebda0311990be413e44132f74560722a3
Yuming <rqmu0114@gmail.com>
2022-07-03 10:46:30 +0800

    assignment-3 upload

---
+++ week-4/assignment-3/src/component/ContentBox.js
@@ -0,0 +1,12 @@
+const ContentBox = () => {
+  return (
+    <div className="content-box">
+      <div className="box">Content Box 1</div>
+      <div className="box">Content Box 2</div>
+      <div className="box">Content Box 3</div>
+      <div className="box">Content Box 4</div>
+    </div>
+  );
+};
+
+export default ContentBox
---
+++ week-4/assignment-3/src/component/Sidebar.js
@@ -0,0 +1,22 @@
+
+const Sidebar = () => {
+
+  return (
+    <ul className="menu">
+      <li>
+        <a href="#">Item1</a>
+      </li>
+      <li>
+        <a href="#">Item2</a>
+      </li>
+      <li>
+        <a href="#">Item3</a>
+      </li>
+      <li>
+        <a href="#">Item4</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Sidebar;
